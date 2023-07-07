8e55926f5a8b975aee5a05d78ad9f16965e1c8ac
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2023-07-07 15:42:25 +0530

    Working prototype

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
+const _ = require("lodash");
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
@@ -71,10 +72,13 @@ async function authorize() {
  */
 async function listEvents(auth) {
     const calendar = google.calendar({version: 'v3', auth});
+    const start = new Date(2023, 6, 10);
+    const end = new Date(2023, 6, 15);
     const res = await calendar.events.list({
         calendarId: 'primary',
-        timeMin: new Date().toISOString(),
-        maxResults: 10,
+        timeMin: start.toISOString(),
+        timeMax: end.toISOString(),
+        maxResults: 200,
         singleEvents: true,
         orderBy: 'startTime',
     });
@@ -83,11 +87,44 @@ async function listEvents(auth) {
         console.log('No upcoming events found.');
         return;
     }
-    console.log('Upcoming 10 events:');
-    events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-    });
+    console.log(`Upcoming ${events.length} events from ${start} to ${end}:`);
+    accepted_events = events.filter(event => {
+        me = event.attendees.find(attendee => attendee.self)
+        return "accepted" === me.responseStatus
+    })
+
+    eventsGroupedByTags = _.groupBy(accepted_events, event => {
+        me = event.attendees.find(attendee => attendee.self)
+        return me.comment
+    })
+
+    _.forEach(eventsGroupedByTags, (group, tag) => {
+        groupDuration = group.reduce(function(total, currentEvent) {
+            const start = new Date(currentEvent.start.dateTime || currentEvent.start.date);
+            const end = new Date(currentEvent.end.dateTime || currentEvent.end.date);
+            const duration = (end - start) / (1000 * 60 * 60);
+            // console.log(`${duration}`)
+            return total + duration
+        }, 0)
+
+        console.log(`${tag} => ${JSON.stringify(groupDuration)}`)
+    })
+    // durationsByTag = eventsGroupedByTags.map(group => group.reduce(function(total, currentEvent) {
+    //     const start = new Date(currentEvent.start.dateTime || currentEvent.start.date);
+    //     const end = new Date(currentEvent.end.dateTime || currentEvent.end.date);
+    //     const duration = (end - start) / (1000 * 60 * 60);
+    //     return total + duration
+    // }))
+
+    // console.log(durationsByTag)
+    // accepted_events.map((event, i) => {
+    //     const start = new Date(event.start.dateTime || event.start.date);
+    //     const end = new Date(event.end.dateTime || event.end.date);
+    //     const duration = (end - start) / (1000 * 60 * 60);
+    //     me = event.attendees.find(attendee => attendee.self)
+    //     // console.log(JSON.stringify(event))
+    //     console.log(`${start} - ${end} (${duration}) -> ${event.summary} - ${me.responseStatus} - ${me.comment}`);
+    // });
 }
 
 authorize().then(listEvents).catch(console.error);
